1c3baae5bb744063df4d177c4737da1552986b59
Wim Fabri <wim.fabri@xenit.eu>
2021-09-06 16:52:11 +0200

    XENOPS-882 - fix typo

--- src/main/docker/root/docker-entrypoint.sh
+++ src/main/docker/root/docker-entrypoint.sh
@@ -22,7 +22,7 @@ ln -s /config/patronictl.yaml /root/.config/patroni/patronictl.yaml
 if [[ -d "/config/patroni" ]]; then
   exec gosu postgres /patroni.py /config/patroni
 elif [[ -f "/config/patroni.yml" ]]; then
-  exec gosu postgres /patroni.py /config/patroni.yaml
+  exec gosu postgres /patroni.py /config/patroni.yml
 else
   echo "ERROR - either config directory /config/patroni or config file /config/patroni.yaml should exist"
   exit 1
